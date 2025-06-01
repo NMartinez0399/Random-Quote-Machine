de125707549f7399c7bd323180854314c1316f34
NMartinez0399 <97861596+NMartinez0399@users.noreply.github.com>
2025-06-01 12:02:09 -0700

    init repo

---
+++ src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import { BookCover } from "../components/BookCover";
+
+export const Home = () => {
+    return (
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+            {/* Header */}
+
+            {/* Quote Generator */}
+
+            {/* Book Cover */}
+            <BookCover />
+
+            {/* Tweet */}
+
+            {/* Footer */}
+        </div>
+    );
+}
