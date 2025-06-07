3cef1f2b8101b48f12b7db82ed960d5e47851e15
NMartinez0399 <97861596+NMartinez0399@users.noreply.github.com>
2025-06-07 11:10:39 -0700

    added tweet functionality

--- src/components/PageContainer.tsx
+++ src/components/PageContainer.tsx
@@ -1,3 +1,5 @@
+import { SocialIcon } from "react-social-icons";
+
 interface Quote {
     quote: string;
     author: string;
@@ -73,6 +75,11 @@ export const PageContainer = () => {
         newQuote.addEventListener('click', handleButtonClick);
     }
 
+    const handleTwitterShare = () => {
+        const authorQuote = document.getElementById('author-quote') as HTMLParagraphElement;
+        window.open(`https://twitter.com/intent/tweet?text=${authorQuote.textContent}`, '_blank');
+    }
+
     return (
         <section className='relative'>
             <div className='container mx-auto max-w-5xl'>
@@ -82,33 +89,41 @@ export const PageContainer = () => {
                             <h1 className='asbolute text-6xl font-medium text-primary animate-slide'>
                                 Random
                             </h1>
-                            <h1 className='asbolute text-6xl font-medium text-primary animate-slide'>
+                            <h1 className='asbolute text-6xl font-medium text-primary animate-slide-delay-1'>
                                 Quote
                             </h1>
-                            <h1 className='asbolute text-6xl font-medium text-primary animate-slide'>
+                            <h1 className='asbolute text-6xl font-medium text-primary animate-slide-delay-2'>
                                 Machine
                             </h1>
-                            <div className='container m-auto py-10'>
+                            <div className='container flex flex-col m-auto py-10 space-y-4 opacity-0 animate-fade-in'>
                                 <button
-                                    className='button-style'
+                                    className='quote-button min-h-16'
                                     id='new-quote-btn'
                                     type='submit'
                                     onClick={handleButtonClick}
                                 >
-                                    Generate Quote
+                                    Generate Quote 
+                                </button>
+                                <button 
+                                    className='share-button'
+                                    id='share-quote-btn'
+                                    type = 'submit'
+                                    onClick={handleTwitterShare}
+                                >
+                                    Share Quote {<SocialIcon url='https://twitter.com' />}
                                 </button>
                             </div>
                         </div>
                     </section>
                     <section className='flex flex-col items-center h-screen max-h-screen justify-center relative'>
-                        <div className='w-full mx-auto max-w-7xl relative'>
-                            <div className='max-w-sm mx-auto bookCover'>
+                        <div className='w-full mx-auto max-w-7xl relative opacity-0 animate-fade-in'>
+                            <div className='max-w-l mx-auto bookCover'>
                                 <div className='grid grid-cols-12 text-secondary'>
                                     <div className='col-span-1 flex border-r border-gray-400 py-64'></div>
-                                    <div className='col-span-11 flex flex-col justify-between p-6'>
+                                    <div className='col-span-11 flex flex-col justify-between p-4'>
                                         <div className='mt-12 text-left space-y-8'>
                                             <p
-                                                className='text-xl mt-3 lg:text-6xl font-medium leading-none overflow-hidden'
+                                                className='text-3xl mt-3 lg:text-4xl font-medium leading-none min-h-8/10 text-pretty md:text-balance overflow-scroll'
                                                 id='author-quote'
                                             >
                                                 Author Quote
