d2673a45d7638a4e68c9f3069214abe4ad525295
NMartinez0399 <97861596+NMartinez0399@users.noreply.github.com>
2025-06-07 11:18:37 -0700

    fixed mpb run buil error

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, type Alias } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import * as path from 'path'
